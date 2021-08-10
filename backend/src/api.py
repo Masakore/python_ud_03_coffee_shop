2a673fd2954e733c0b6726d1fce59527a3097e94
Masayuki Osuka <maskore8@gmail.com>
2021-08-11 07:57:02 +0800

    All todo's done

--- backend/src/api.py
+++ backend/src/api.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
+from werkzeug.exceptions import NotFound, UnprocessableEntity
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
@@ -17,7 +18,7 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -30,6 +31,30 @@ CORS(app)
 '''
 
 
+@app.route('/drinks', methods=['GET'])
+def get_short_drinks():
+    try:
+        result = Drink.query.all()
+
+        if len(result) == 0:
+            abort(404)
+
+        drinks = []
+        for drink in result:
+            drinks.append(drink.short())
+
+        return jsonify({
+            "success": True,
+            "drinks": drinks
+        })
+
+    except NotFound:
+        abort(404)
+
+    except Exception:
+        abort(500)
+
+
 '''
 @TODO implement endpoint
     GET /drinks-detail
@@ -38,6 +63,31 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drink-details')
+def get_long_drinks():
+    try:
+        result = Drink.query.all()
+
+        if len(result) == 0:
+            abort(404)
+
+        drinks = []
+        for drink in result:
+            drinks.append(drink.long())
+
+        return jsonify({
+            "success": True,
+            "drinks": drinks
+        })
+
+    except NotFound:
+        abort(404)
+
+    except Exception:
+        abort(500)
 
 
 '''
@@ -49,6 +99,31 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink():
+    try:
+        body = request.get_json()
+        title = body.get('title', None)
+        recipe = body.get('recipe', None)
+
+        if (title is None or recipe is None):
+            abort(422)
+
+        drink = Drink(title=title, recipe=json.dumps([recipe]))
+        drink.insert()
+        return jsonify({
+            "success": True,
+            "drinks": list(drink.long())
+        }), 200
+
+    except UnprocessableEntity:
+        abort(422)
+
+    except Exception:
+        abort(500)
 
 
 '''
@@ -64,6 +139,42 @@ CORS(app)
 '''
 
 
+@app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink(drink_id):
+    try:
+        body = request.get_json()
+        title = body.get('title', None)
+        recipe = body.get('recipe', None)
+
+        if (title is None and recipe is None):
+            abort(422)
+
+        drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+        if drink is None:
+            abort(404)
+
+        if title:
+            drink.title = title
+        if recipe:
+            drink.recipe = recipe
+        drink.update()
+
+        return jsonify({
+            "success": True,
+            "drinks": [drink.long()]
+        }), 200
+
+    except UnprocessableEntity:
+        abort(422)
+
+    except NotFound:
+        abort(404)
+
+    except Exception:
+        abort(500)
+
+
 '''
 @TODO implement endpoint
     DELETE /drinks/<id>
@@ -74,6 +185,28 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(drink_id):
+    try:
+        drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+        if drink is None:
+            abort(404)
+
+        drink.delete()
+
+        return jsonify({
+            "success": True,
+            "delete": drink_id
+        }), 200
+
+    except NotFound:
+        abort(404)
+
+    except Exception:
+        abort(500)
 
 
 # Error Handling
@@ -91,6 +224,15 @@ def unprocessable(error):
     }), 422
 
 
+@app.errorhandler(500)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "internal server error"
+    }), 500
+
+
 '''
 @TODO implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
@@ -101,14 +243,31 @@ def unprocessable(error):
                     }), 404
 
 '''
-
 '''
 @TODO implement error handler for 404
     error handler should conform to general task above
 '''
 
 
+@app.errorhandler(404)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
+
+
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+
+
+@app.errorhandler(AuthError)
+def autherror(error):
+    return jsonify({
+        "success": False,
+        "error": error.status_code,
+        "message": error.error['description']
+    }), error.status_code
---
+++ frontend/README.md
@@ -0,0 +1,35 @@
+# Coffee Shop Frontend
+
+## Getting Setup
+
+### Installing Dependencies
+
+#### Installing Node and NPM
+
+This project depends on Nodejs and Node Package Manager (NPM). Before continuing, you must download and install Node (the download includes NPM) from [https://nodejs.com/en/download](https://nodejs.org/en/download/).
+
+#### Installing Ionic Cli
+
+The Ionic Command Line Interface is required to serve and build the frontend. Instructions for installing the CLI is in the [Ionic Framework Docs](https://ionicframework.com/docs/installation/cli).
+
+#### Installing project dependencies
+
+This project uses NPM to manage software dependencies. NPM Relies on the package.json file located in the `frontend` directory of this repository. After cloning, open your terminal and run:
+
+```bash
+npm install
+```
+
+### Configure Environment Variables
+
+Ionic uses a configuration file to manage environment variables. These variables ship with the transpiled software and should not include secrets.
+
+- Open `./src/environments/environments.ts` and ensure each variable reflects the system you stood up for the backend.
+
+## Running Your Frontend in Dev Mode
+
+Ionic ships with a useful development server which detects changes and transpiles as you work. The application is then accessible through the browser on a localhost port. To run the development server, cd into the `frontend` directory and run:
+
+```bash
+ionic serve
+```
